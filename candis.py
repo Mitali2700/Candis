38524891d773cb14296c3eb9c009d25357aac54a
Mitali2700 <201B155@juetguna.in>
2022-06-03 23:12:47 +0530

    third  commit

--- candis.py
+++ candis.py
@@ -1,5 +1,9 @@
 import pyttsx3
+import speech_recognition as sr
 import datetime
+import wikipedia
+import webbrowser
+
 engine = pyttsx3.init('sapi5')  # Microsoft Speech API
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)  # 1-Hazel 0-Zira
@@ -9,12 +13,13 @@ def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
+
 def wishMe():
     hour = int(datetime.datetime.now().hour)
-    if hour>=0 and hour<12:
+    if hour >= 0 and hour < 12:
         speak("Good Morning!")
 
-    elif hour>=12 and hour<18:
+    elif hour >= 12 and hour < 18:
         speak("Good Afternoon")
 
     else:
@@ -22,10 +27,44 @@ def wishMe():
 
     speak("I am Candis ma'am. Please tell me how may I help you?")
 
+
 def takeCommand():
-    '''
-        It take
-    '''
+    # It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')  # Using google for voice recognition.
+        print(f"User said: {query}\n")  # User query will be printed.
+
+    except Exception as e:
+        # print(e)
+        print("Say that again please...")  # Say that again will be printed in case of improper voice
+        return "None"  # None string will be returned
+    return query
+
 
 if __name__ == "__main__":
     wishMe()
+    while True:
+        query = takeCommand().lower()
+
+        #  logic for executing task based on query
+        if 'wikipedia' in query:  # if wikipedia found in the query then this block will be executed
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+
+
